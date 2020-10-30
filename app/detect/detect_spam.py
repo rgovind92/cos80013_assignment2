e43552b8af52ba43350e92604ca0ec4e7eb78b0f
Govind <xzibithatred@yahoo.com>
2020-10-30 17:45:36 +0000

    Update detect_spam.py
    Formatting

--- app/detect/detect_spam.py
+++ app/detect/detect_spam.py
@@ -1,80 +1,38 @@
-import os
+import pathlib
 import sys
-import pathlib
-import argparse
+import os
+from email.parser import HeaderParser
 from pdb import set_trace
-import string
-import pickle
-import pathlib
+import argparse
 
 import pandas as pd
-import numpy as np
-
-import nltk
-from nltk.corpus import stopwords, words
+import matplotlib.pyplot as plt
 from nltk import FreqDist
-
-from sklearn.feature_extraction.text import CountVectorizer
-from autocorrect import Speller
-from sklearn.naive_bayes import MultinomialNB
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import confusion_matrix, accuracy_score
+import pickle
 
 from .. import (
   read_data_from_dir,
-  train_gibberish_model,
-  avg_transition_prob,
-  replace_gibberish,
-  replace_gibberish_in_spam,
-  visualize_tokens,
-  clean,
-  bar_plot,
+  get_location,
+  get_locations,
+  is_public_ip,
   sort_freq_dist,
-  detokenize,
-  deleet
+  bar_plot,
+  get_hops,
+  extract_headers
 )
 
 pd.options.mode.chained_assignment = None
 
-def pre_process(text):
-  text = ''.join(_.lower() for _ in text if _ not in string.punctuation).split()
-  text = [_ for _ in text if len(_) > 2 and  _.lower() not in stopwords.words('english') and _.isalpha()]
+results_dir = os.path.join('app', 'results', 'trace')
 
-  #stemmer = PorterStemmer()
-  #text = [stemmer.stem(_) for _ in text]
+ham_dir = os.path.join('app', 'data','spam_assassin','ham')
+spam_dir = os.path.join('app', 'data','spam_assassin','spam')
 
-  return ' '.join(text)
+ham_file_path = os.path.join('app','trace','ham.csv')
+spam_file_path = os.path.join('app','trace','spam.csv')
 
-def predict(texts):
-  return classifier.predict(vectorizer.transform([clean(replace_gibberish(text, '', gibberish_matrix, gibberish_threshold)) for text in texts]))
-
-def _predict(texts):
-  return classifier.predict(vectorizer.transform([clean(text) for text in texts]))
-
-def __predict(texts):
-  return classifier.predict(vectorizer.transform([detokenize(deleet(text)) for text in texts]))
-
-def ___predict(texts):
-  return classifier.predict(vectorizer.transform([detokenize(text) for text in texts]))
-
-def ____predict(texts):
-  return classifier.predict(vectorizer.transform(texts))
-
-ham_dir = os.path.join('app', 'data', 'enron', 'ham')
-spam_dir = os.path.join('app', 'data', 'enron', 'spam')
-csv_path = os.path.join('app', 'detect', 'enron.csv')
-gibberish_model_path = os.path.join('app', 'gibberish_detector', 'gibberish_model.pki')
-
-results_dir = os.path.join('app', 'results', 'detect')
-
-hammy_words_viz = os.path.join(results_dir, 'hammy_words.png')
-spammy_words_viz = os.path.join(results_dir, 'spammy_words.png')
-ham_word_size_viz = os.path.join(results_dir, 'ham_word_size.png')
-spam_word_size_viz = os.path.join(results_dir, 'spam_word_size.png')
-
-results_path = os.path.join(results_dir, 'results.csv')
-false_positives_results_path = os.path.join(results_dir, 'false_positive_results.csv')
-false_negatives_results_path = os.path.join(results_dir, 'false_negative_results.csv')
+ham_file = pathlib.Path(ham_file_path)
+spam_file = pathlib.Path(spam_file_path)
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--ham', help='path to the directory that contains ham emails')
@@ -90,129 +48,107 @@ if args.spam:
   if pathlib.Path(os.path.abspath(args.spam)).is_dir():
     spam_dir = args.spam
 
-if not pathlib.Path(gibberish_model_path).is_file():
+print('\n\n')
+
+# If ham or spam csv does not exist, read it recursively from
+# its respective directories, write the result to a csv,
+# and use that csv in subsequent runs
+
+if ham_file.is_file():
+  print('\n\nReading data from ' + ham_file_path + '...')
+  ham = pd.read_csv(ham_file_path)
+else:
   try:
-    print('\nGibberish model not found at ' + gibberish_model_path + '! Training it now...')
-    train_gibberish_model()
+    print('\n\n' + ham_file_path + ' not found! Reading from ' + ham_dir)
+    ham = read_data_from_dir(ham_dir)
   except:
-    print('\nAn error occurred while training the gibberish model')
+    print('Ham folder (' + ham_dir + ') could not be accessed!\n')
     sys.exit(1)
 
-gibberish_model = pickle.load(open(gibberish_model_path, 'rb'))
-gibberish_matrix = gibberish_model['mat']
-gibberish_threshold = gibberish_model['thresh']
+  ham.to_csv(ham_file_path)
+  print('\n\nData read successfully and written to ' + ham_file_path
 
-print('\nGibberish model loaded!')
-
-if not pathlib.Path(csv_path).is_file():
-  print('\n\n' + csv_path + ' not found! Reading from ' + spam_dir + ' and ' + ham_dir)
-  data = read_data_from_dir(spam_dir, 'spam')
-  data = read_data_from_dir(ham_dir, 'ham', data)
-  data['text'] = data['text'].apply(pre_process)
-  data.to_csv(csv_path)
-  print('Data read successfully and written to ' + csv_path)
-
+if spam_file.is_file():
+  print('\n\nReading data from ' + spam_file_path + '...')
+  spam = pd.read_csv(spam_file_path)
 else:
-  print('\n\nReading data from ' + csv_path + '...')
-  data = pd.read_csv(csv_path)
+  try:
+    print('\n\n' + spam_file_path + ' not found! Reading from ' + spam_dir)
+    spam = read_data_from_dir(spam_dir)
+  except:
+    print('Spam folder could not be accessed!\n')
+    sys.exit(1)  
 
-spam_text = ' '.join(data[data['label'] == 'spam']['text'])
-ham_text = ' '.join(data[data['label'] == 'ham']['text'])
+  print('\n\nData read successfully and written to ' + spam_file_path
+  spam.to_csv(spam_file_path)
 
-spam_tokens = nltk.word_tokenize(spam_text)
-ham_tokens = nltk.word_tokenize(ham_text)
+print('\n\nExtracting headers...')
 
-spam_word_lengths_dist = FreqDist([len(_) for _ in spam_tokens])
-ham_word_lengths_dist = FreqDist([len(_) for _ in ham_tokens])
+ham_headers = [extract_headers(HeaderParser().parsestr(_), ['Received', 'From']) for _ in ham['text']]
+spam_headers = [extract_headers(HeaderParser().parsestr(_), ['Received', 'From']) for _ in spam['text']]
 
-print('\nAverage size of words in spam mails: ' +  str(sum([len(_) for _ in spam_tokens]) / len(spam_tokens)))
-print('\nAverage size of words in ham mails: ' +  str(sum([len(_) for _ in ham_tokens]) / len(ham_tokens)))
+ham_headers_received_len_freq_dist = FreqDist([len(_['Received']) for _ in ham_headers])
+spam_headers_received_len_freq_dist = FreqDist([len(_['Received']) for _ in spam_headers])
+
+ham_hop_locations = [get_locations(get_hops(_)) for _ in ham_headers]
+spam_hop_locations = [get_locations(get_hops(_)) for _ in spam_headers]
+
+ham_hop_countries = [[_['country_name'] for _ in __] for __ in ham_hop_locations]
+spam_hop_countries = [[_['country_name'] for _ in __] for __ in spam_hop_locations]
+
+ham_hop_countries_uniq = [len(set(_)) for _ in ham_hop_countries]
+spam_hop_countries_uniq = [len(set(_)) for _ in spam_hop_countries]
+
+ham_origin_countries = [_[-1] if _ and len(_) > 0 else 'Unknown' for _ in ham_hop_countries]
+spam_origin_countries = [_[-1] if _ and len(_) > 0 else 'Unknown' for _ in spam_hop_countries]
+
+ham_origin_addresses = [(_[-1]['city'] or 'Unknown city') + ', ' + (_[-1]['state'] or 'Unknown state') + ', ' + (_[-1]['country_name'] or 'Unknown country') if len(_) > 0 else 'Unknown' for _ in ham_hop_locations]
+spam_origin_addresses = [(_[-1]['city'] or 'Unknown city') + ', ' + (_[-1]['state'] or 'Unknown state') + ', ' + (_[-1]['country_name'] or 'Unknown country') if len(_) > 0 else 'Unknown' for _ in spam_hop_locations]
+
+ham_origin_countries_freq_dist = FreqDist(ham_origin_countries)
+spam_origin_countries_freq_dist = FreqDist(spam_origin_countries)
+
+ham_hop_countries_uniq_freq_dist = FreqDist(ham_hop_countries_uniq)
+spam_hop_countries_uniq_freq_dist = FreqDist(spam_hop_countries_uniq)
 
 bar_plot(
-  *list(zip(*spam_word_lengths_dist.most_common(10))),
-  'Word size',
-  'Number of words',
-  'Plot of the sizes of words in spam mails',
-  spam_word_size_viz
+  *sort_freq_dist(ham_headers_received_len_freq_dist),
+  'Number of hops',
+  'Number of emails',
+  'Plot of number of Received headers in ham emails',
+  os.path.join(results_dir, 'ham_received.png')
+)
+bar_plot(
+  *sort_freq_dist(spam_headers_received_len_freq_dist),
+  'Number of hops',
+  'Number of emails',
+  'Plot of number of Received headers in spam emails',
+  os.path.join(results_dir, 'spam_received.png')
 )
 
 bar_plot(
-  *list(zip(*ham_word_lengths_dist.most_common(10))),
-  'Word size',
-  'Number of words',
-  'Plot of the sizes of words in ham mails',
-  ham_word_size_viz
+  *list(zip(*ham_hop_countries_uniq_freq_dist.most_common(10))),
+  'Number of unique countries hopped',
+  'Number of emails',
+  'Plot of the number of unique countries through which ham emails hopped',
+  os.path.join(results_dir, 'ham_hop_unique_countries.png')
 )
 
-visualize_tokens(ham_text, save_to=hammy_words_viz)
-visualize_tokens(spam_text, save_to=spammy_words_viz)
-
-# Change 1
-data = replace_gibberish_in_spam(data, gibberish_matrix, gibberish_threshold)
-
-vectorizer = CountVectorizer()
-sparse = vectorizer.fit_transform(data['text'])
-
-feature_names = vectorizer.get_feature_names()
-
-indices = np.arange(len(data))
-
-x_train, x_test, y_train, y_test, idx_train, idx_test = train_test_split(
-  sparse,
-  data['label'],
-  indices,
-  test_size=0.20,
-  random_state=0
+bar_plot(
+  *list(zip(*spam_hop_countries_uniq_freq_dist.most_common(10))),
+  'Number of unique countries hopped',
+  'Number of emails',
+  'Plot of the number of unique countries ehrough which spam emails hopped',
+  os.path.join(results_dir, 'spam_hop_unique_countries.png')
 )
 
-classifier = MultinomialNB()
-print('\n\nFitting the model...')
-classifier.fit(x_train, y_train)
+ham = ham[['name']]
+spam = spam[['name']]
 
-pred = classifier.predict(x_test)
+ham['origin_address'] = ham_origin_addresses
+spam['origin_address'] = spam_origin_addresses
 
-print('\n\nPrediction complete! Results:\n\n')
-print('Confusion matrix\n', confusion_matrix(y_test, pred))
-print('\n\nAccuracy: ', accuracy_score(y_test, pred))
+ham.to_csv(os.path.join(results_dir, 'ham_origins.csv'))
+spam.to_csv(os.path.join(results_dir, 'spam_origins.csv'))
 
-out_df = pd.DataFrame()
-
-for i in range(len(pred)):
-  out_df = out_df.append(data.loc[idx_test[i]])
-
-out_df['predicted_label'] = pred
-out_df.to_csv(results_path)
-
-out_false_positives_df = out_df[out_df['label'] == 'ham'][out_df['predicted_label'] == 'spam']
-out_false_negatives_df = out_df[out_df['label'] == 'spam'][out_df['predicted_label'] == 'ham']
-out_false_positives_df.to_csv(false_positives_results_path)
-out_false_negatives_df.to_csv(false_negatives_results_path)
-
-print('\n\nResults written to ' + results_path)
-
-# Defence tests:
-examples = [
-  #'Win a chance to receive free gifts today! Valid for a limited time only!',
-  'W*i*n a c*h*a*n*c*e t*o r*e*c*e*i*v*e f*r*e*e g*i*f*t*s t*o*d*a*y*! V*a*l*i*d f*o*r a l*i*m*i*t*e*d t*i*m*e o*n*l*y', # tokenization
-  'Ԝіո а сhаոсе tο rесеіⅴе frее ցіftѕ tοⅾау! Ꮩаⅼіⅾ fоr а ⅼіⅿіtеⅾ tіⅿе оոⅼу!', # invisible obfuscation
-  'Ԝіո а сḣаոсе tο rесеіⅴе frее ģіftѕ tοḋау! Ꮩаⅼіḋ fоr а ⅼіⅿіtеḋ tіⅿе оոⅼу!', # weak obfuscation
-  'Ẃɩռ ắ ĉẖӑῄćḛ ŧợ ӷȩϛḝίὗę ẛřȅḗ ģῑẛṭś ȶʘḓǻӳ! Ѵǎḻἳḑ ẛổȑ ȁ ƚίṃīṯëɗ էίṃē ṑṉḻӯ!', # medium obfuscation
-  'Ẃῑῇ ἀ ¢ḫἁἧċé ṯȏ ɼεçėȉύȇ ƒȓēϱ գǐẛեṡ ṫṍƌȃӳ! Ѷǡŀі₫ ḟṓг ἇ ƚίɱὶէёḍ †ỉḿḛ ӧηɭγ!', # strong obfuscation
-  'W1n 4 ch4nc3 t0 r3c31v3 fr33 g1fts t0d4y! V4l1d f0r a l1m1t3d t1m3 0nly!', # leet obfuscation
-  'skadnmaskfm sakfmkamf ksafmkasm safasmki ksfmkfsma kafsnksfm Win a chance to receive free gifts today! Valid for a limited time only!' # weak statistical
-]
-
-print('\nPredictions without sanitization:')
-print(____predict(examples))
-
-print('\nPredictions with detokenization:')
-print(___predict(examples))
-
-print('\nPredictions with detokenization and deleetion:')
-print(__predict(examples))
-
-print('\nPredictions with detokenization, deleetion, and deunicodification:')
-print(_predict(examples))
-
-print('\nPredictions with detokenization, deleetion, deunicodification, and gibberish removal:')
-print(predict(examples))
+print('\n\nSuccess! Results have been saved to ' + results_dir)
