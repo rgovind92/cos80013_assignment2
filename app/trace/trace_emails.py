8aedf6155d7bbaf37ab2e04bc3b6b9a63d0dfc45
Govind <xzibithatred@yahoo.com>
2020-10-31 07:54:35 +0000

    Update trace_emails.py

--- app/trace/trace_emails.py
+++ app/trace/trace_emails.py
@@ -1,9 +1,10 @@
 import pathlib
 import sys
 import os
-
 from email.parser import HeaderParser
 from pdb import set_trace
+import argparse
+
 import pandas as pd
 import matplotlib.pyplot as plt
 from nltk import FreqDist
@@ -27,37 +28,59 @@ results_dir = os.path.join('app', 'results', 'trace')
 ham_dir = os.path.join('app', 'data','spam_assassin','ham')
 spam_dir = os.path.join('app', 'data','spam_assassin','spam')
 
-ham_file_path = os.path.join('app', 'data','spam_assassin','ham.csv')
-spam_file_path = os.path.join('app', 'data','spam_assassin','spam.csv')
+ham_file_path = os.path.join('app','trace','ham.csv')
+spam_file_path = os.path.join('app','trace','spam.csv')
 
 ham_file = pathlib.Path(ham_file_path)
 spam_file = pathlib.Path(spam_file_path)
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--ham', help='path to the directory that contains ham emails')
+parser.add_argument('--spam', help='path to the directory that contains spam emails')
+
+args = parser.parse_args()
+
+if args.ham:
+  if pathlib.Path(os.path.abspath(args.ham)).is_dir():
+    ham_dir = args.ham
+
+if args.spam:
+  if pathlib.Path(os.path.abspath(args.spam)).is_dir():
+    spam_dir = args.spam
 
 # If ham or spam csv does not exist, read it recursively from
 # its respective directories, write the result to a csv,
 # and use that csv in subsequent runs
 
 if ham_file.is_file():
+  print('\nReading data from ' + ham_file_path + '...')
   ham = pd.read_csv(ham_file_path)
 else:
   try:
+    print('\n\n' + ham_file_path + ' not found! Reading from ' + ham_dir)
     ham = read_data_from_dir(ham_dir)
   except:
     print('Ham folder (' + ham_dir + ') could not be accessed!\n')
     sys.exit(1)
 
   ham.to_csv(ham_file_path)
+  print('\nData read successfully and written to ' + ham_file_path)
 
 if spam_file.is_file():
+  print('\nReading data from ' + spam_file_path + '...')
   spam = pd.read_csv(spam_file_path)
 else:
   try:
+    print('\n\n' + spam_file_path + ' not found! Reading from ' + spam_dir)
     spam = read_data_from_dir(spam_dir)
   except:
     print('Spam folder could not be accessed!\n')
     sys.exit(1)  
 
+  print('\n\nData read successfully and written to ' + spam_file_path)
   spam.to_csv(spam_file_path)
+
+print('\n\nExtracting headers...')
 
 ham_headers = [extract_headers(HeaderParser().parsestr(_), ['Received', 'From']) for _ in ham['text']]
 spam_headers = [extract_headers(HeaderParser().parsestr(_), ['Received', 'From']) for _ in spam['text']]
@@ -90,14 +113,14 @@ bar_plot(
   *sort_freq_dist(ham_headers_received_len_freq_dist),
   'Number of hops',
   'Number of emails',
-  'Plot of number of Received headers in ham emails',
+  'Plot of the number of servers through which ham emails hopped',
   os.path.join(results_dir, 'ham_received.png')
 )
 bar_plot(
   *sort_freq_dist(spam_headers_received_len_freq_dist),
   'Number of hops',
   'Number of emails',
-  'Plot of number of Received headers in spam emails',
+  'Plot of the number of servers through which spam emails hopped',
   os.path.join(results_dir, 'spam_received.png')
 )
 
@@ -113,7 +136,7 @@ bar_plot(
   *list(zip(*spam_hop_countries_uniq_freq_dist.most_common(10))),
   'Number of unique countries hopped',
   'Number of emails',
-  'Plot of the number of unique countries ehrough which spam emails hopped',
+  'Plot of the number of unique countries through which spam emails hopped',
   os.path.join(results_dir, 'spam_hop_unique_countries.png')
 )
 
@@ -126,4 +149,5 @@ spam['origin_address'] = spam_origin_addresses
 ham.to_csv(os.path.join(results_dir, 'ham_origins.csv'))
 spam.to_csv(os.path.join(results_dir, 'spam_origins.csv'))
 
-print('Success! Results have been saved to ' + results_dir)
+print('\n\nSuccess! Results have been saved to ' + results_dir + '\n')
+
